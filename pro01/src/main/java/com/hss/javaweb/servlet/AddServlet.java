3f1f14521efb787ca9836ff233b519017be7a61a
Kiki <1351090471@qq.com>
2022-04-04 21:18:31 +0800

    Initial commit

---
+++ pro01/src/main/java/com/hss/javaweb/servlet/AddServlet.java
@@ -0,0 +1,29 @@
+package com.hss.javaweb.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Post 方式下，设置编码，防止中文乱码
+        req.setCharacterEncoding("UTF-8");
+        String fname = req.getParameter("fname");
+        String priceStr = req.getParameter("price");
+        Integer price = Integer.parseInt(priceStr);
+        String fcount = req.getParameter("fcount");
+        String remark = req.getParameter("remark");
+
+        System.out.println("fname = " + fname);
+        System.out.println("price = " + price);
+        System.out.println("fcount = " + fcount);
+        System.out.println("remark = " + remark);
+
+
+
+    }
+}
